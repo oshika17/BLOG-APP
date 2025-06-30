df95721512500abe8c38d7041a0e302477b97b3e
oshika17 <oshikasaini01@gmail.com>
2025-06-30 18:10:56 +0530

    blog app

---
+++ src/pages/Login.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Login as Logincompo } from '../components'
+function Login(){
+    return(
+        <div className='py-8'>
+            <Logincompo />
+         </div>
+    )
+}
+export default Login
